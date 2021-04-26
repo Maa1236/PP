ce70f2eceb35b1c92f720e88db48047d0a90bd81
Maa1236 <37353149+Maa1236@users.noreply.github.com>
2021-04-27 00:43:25 +0200

    Revert "Development functions"

---
+++ nova-vezba.js
@@ -0,0 +1,18 @@
+var result = '';
+var a ="3";
+
+if (a>2) {
+    result = a + ' is greater than 2';
+}
+
+console.log(result);
+
+// to check if variable is defined
+var result ='';
+if (typeof undeclaredVariable !=="undefined") {
+    result = "yes";
+}
+console.log(result);
+
+
+
