e4e169ce229daa45f0d2c58c849c8c3dfc8da724
Marija <37353149+Maa1236@users.noreply.github.com>
2021-05-06 00:05:31 +0200

    f-ions8 finished

--- functions/functions8.js
+++ functions/functions8.js
@@ -40,7 +40,7 @@ number of replacements.
 Input: prograMming
 Output: progra**ing, 2*/
 
-function replaceAppLetter(string, letter, replacement) {
+(function replaceAppLetter(string, letter, replacement) {
     var res = "",
         tempArr = [],
         counter = 0,
@@ -54,9 +54,41 @@ function replaceAppLetter(string, letter, replacement) {
         }
         res += tempArr[i];
     }
-    return res + ", " + counter;
-}
-console.log(replaceAppLetter("prograMming", "m", "*"));
+    return console.log(res + ", " + counter);
+}) ("prograMming", "m", "*");
+
+/*4. Write a function with parameters name and surname that returns a function that
+suggests an email in the form name.surname@gmail.com.
+Input: pera peric
+Output: pera.peric@gmail.com*/
+
+(function suggestEmail(name, surname) {
+    return function() {
+        console.log(name + "." + surname + "@gmail.com");
+    }
+})("pera", "peric")();
+
+/*6.Write a function that checks if a given string is valid password. The password is valid if it
+is at least 6 characters long and contains at least one digit. The function should receive
+two callbacks named successCallback and errorCallback that should be called in case
+password is correct or invalid.
+Input: JSGuru
+Output: Your password is invalid!
+Input: JSGuru123
+Output: Your password is cool!
+*/
+(function checkPW(password, successCallback, errorCallback) {
+    var digits = ["0", "1", " 2", "3", "4", "5", "6", "7", "8", "9"];
+    for (i = 0; i < password.length; i++) {
+        for (j = 0; j < digits.length; j++) {
+            if (password.length >= 6 && password[i] === digits[j]) {
+                return successCallback();
+            }
+        }
+    }
+    return errorCallback();
+})("JS1234vs3", function() { console.log("Your password is cool") }, function() { console.log("Your password is invalid") });
+
 
 /*7. Write a function that filters elements of the given array so that they satisfy a condition
 given by the callback function.
@@ -64,14 +96,12 @@ Input: [2, 8, 11, 4, 9, 3], callback function checks if the number is odd
 Output: [11, 9, 3]
 */
 
-function filter(array, action) {
+(function filter(array, action) {
     var filteredArray = [];
     for (var i = 0; i < array.length; i++) {
         if (action(array[i])) {
             filteredArray[filteredArray.length] = array[i];
         }
     }
-    return filteredArray;
-}
-var output = filter([2, 8, 11, 4, 9, 3], function(a) { return a % 2 !== 0; });
-console.log(output);
+    return console.log(filteredArray);
+}) ([2, 8, 11, 4, 9, 3], function(a) { return a % 2 !== 0; });
